513a9f60675705372638b6f37d7785d38b5e2ea1
Jacquessl <97961605+Jacquessl@users.noreply.github.com>
2024-06-06 19:33:34 +0200

    Delete WebApplication2 directory

--- WebApplication2/WebApplication2/DTOs/ClientDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication2.DTOs;
-
-public class ClientDTO
-{
-    public string FirstName { get; set; } = null!;
-
-    public string LastName { get; set; } = null!;
-}
--- WebApplication2/WebApplication2/Repositories/IClientRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication2.Repositories;
-
-public interface IClientRepository
-{
-    Task<bool> CanDeleteClientAsync(int clientId);
-    Task DeleteClientAsync(int clientId);
-    Task<bool> ClientExistsByPeselAsync(string pesel);
-}
